d762b4394a87e4de2611f055efa70a140f543ec9
Christopher Simerle <christopher@getmimo.com>
2018-10-19 08:39:24 +0200

    extended webpack config, added monaco editor + webpack plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'monaco-webpack-test';
+  @ViewChild('container')
+  container;
+
+  ngOnInit(): void {
+    monaco.editor.defineTheme('theme', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [{ background: '2f3d47', token: '' }],
+      colors: {
+        'editor.background': '#2f3d47',
+        'editor.lineHighlightBackground': '#d3d3d331'
+      }
+    });
+    monaco.editor.setTheme('theme');
+    monaco.editor.create(this.container.nativeElement, {
+      value: 'console.log("Hello, world")',
+      language: 'javascript'
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,24 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+(<any>window).MonacoEnvironment = {
+  getWorkerUrl: function(moduleId, label) {
+    if (label === 'json') {
+      return './json.worker.bundle.js';
+    }
+    if (label === 'css') {
+      return './css.worker.bundle.js';
+    }
+    if (label === 'html') {
+      return './html.worker.bundle.js';
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return './ts.worker.bundle.js';
+    }
+    return './editor.worker.bundle.js';
+  }
+};
 
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
